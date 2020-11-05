21130091938caa59955f3d1ddf34365bd1136010
Pierce McGeough <pmcgeough@hotmail.co.uk>
2020-11-05 23:49:33 +0000

    WIP

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -15,7 +15,7 @@ export default function LinkCreateForm({ refreshLinks }) {
         e.preventDefault();
         const body = { name, url, description };
         try {
-            await fetch('/api/createLink', {
+            await fetch('/.netlify/functions/createLink', {
                 method: 'POST',
                 body: JSON.stringify(body)
             })
